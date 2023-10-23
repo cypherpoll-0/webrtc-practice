1f749cbdd51c59d44569ed7b0cb01b5342ebe21d
Ayush Kedia <ayushkedia1990@gmail.com>
2023-10-23 18:55:45 +0530

    app changes

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
-const express = require('express')
-const http = require('http')
-const app = express()
-const server = http.createServer(app)
-const socket = require('socket.io')
-const io = socket(server)
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"],
+	},
+});
 
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
 
-io.on('connection', (socket) => {
-    socket.emit('me', socket.id)
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callended");
+	});
 
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('callended')
-    })
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emi("callUser", {
+			signal: data.signalData,
+			from: data.from,
+			name: data.name,
+		});
+	});
 
-    socket.on('callUser', (data) => {
-        io.to(data.userToCall).emi('callUser', {signal: data.signalData, from:data.from, name:data.name})
-    })
-
-    socket.on('answerCall', (data) => {
-        io.to(data.to).emit('callAccepted')
-    }, data.signal)
-})
+	socket.on(
+		"answerCall",
+		(data) => {
+			io.to(data.to).emit("callAccepted");
+		},
+		data.signal
+	);
+});
 
 //just to remove node module
 
-
-server.listen(5000, () => console.log("server is running"))
+server.listen(5000, () => console.log("server is running"));
